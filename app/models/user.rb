b8c6e71dc8b7d29a570141e08d2e546236a59d1b
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-07-29 18:35:00 -0700

    AASM & state machine for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,33 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  include AASM
+
   devise :confirmable, :database_authenticatable, :lockable,
          :invitable, :registerable, :recoverable, :rememberable,
          :timeoutable, :trackable, :validatable
+
+  aasm column: :status, whiny_transitions: false, whiny_persistence: true do
+    state :pending, initial: true
+    state :active
+    state :disabled
+
+    event :activate do
+      transitions from: :pending, to: :active
+    end
+
+    event :disable do
+      transitions from: %i[pending active], to: :disabled
+    end
+
+    event :enable do
+      transitions from: :disabled, to: :active
+    end
+  end
+
+private
+
+  def after_confirmation
+    user.activate! unless user.active?
+  end
 end
