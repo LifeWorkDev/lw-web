eb3c9491efe704a8545ab7102048a551d17c6912
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-08-20 19:12:13 -0700

    only preload_app for puma in production to try to avoid reloading issues

--- config/puma.rb
+++ config/puma.rb
@@ -2,8 +2,6 @@ environment ENV.fetch('RAILS_ENV') { 'development' }
 port        ENV.fetch('PORT') { 3000 }
 workers 1
 
-preload_app!
-
 if Rails.env.development?
   # Allow puma to be restarted by `rails restart` command.
   plugin :tmp_restart
@@ -13,4 +11,6 @@ if Rails.env.development?
     # Trick Webpack into reloading browser
     `touch app/javascript/packs/application.js`
   end
+else
+  preload_app!
 end
