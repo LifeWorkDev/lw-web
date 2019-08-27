0ba7fde0afdb5ebf62486fcc924995fbd4f863c9
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-08-26 17:34:08 -0700

    Update gems & packages, notably eslint-loader to 3.0 & add custom config so eslint-loader cache is busted when .eslintrc changes

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -3,11 +3,16 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 const merge = require('webpack-merge')
 const environment = require('./environment')
 
+// Get the object hash for our single eslintrc to use as a cache identifier
+const objectHash = require('object-hash')
+const eslintCacheIdentifier = objectHash(require('../../.eslintrc.js'));
 environment.loaders.append('eslint', {
   enforce: 'pre',
   exclude: /node_modules/,
   loader: 'eslint-loader',
   options: {
+    cache: true,
+    cacheIdentifer: eslintCacheIdentifier,
     emitWarning: true,
     fix: true,
   },
