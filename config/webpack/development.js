b38a16890a41f7d491b16ed7b97359d2bbbd1c72
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-08-28 17:01:18 -0700

    ESLint fix

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -5,7 +5,7 @@ const environment = require('./environment')
 
 // Get the object hash for our single eslintrc to use as a cache identifier
 const objectHash = require('object-hash')
-const eslintCacheIdentifier = objectHash(require('../../.eslintrc.js'));
+const eslintCacheIdentifier = objectHash(require('../../.eslintrc.js'))
 environment.loaders.append('eslint', {
   enforce: 'pre',
   exclude: /node_modules/,
