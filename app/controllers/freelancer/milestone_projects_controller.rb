acdce1d7ef122fdb43bcc3494ab3aa21779ee85c
Muhammad Soban Akram <soban.se@gmail.com>
2019-09-05 21:48:24 +0500

    freelancer milestone project form added

--- app/controllers/freelancer/milestone_projects_controller.rb
+++ app/controllers/freelancer/milestone_projects_controller.rb
@@ -1,5 +1,27 @@
 module Freelancer
   class MilestoneProjectsController < ProjectsController
+    before_action :set_project, only: %i[edit milestones payments update destroy]
+
+    # GET /milestone_projects/new
+    def new
+      # Convert to https://stackoverflow.com/a/45740056/337446
+      @project = current_user.projects.build(type: MilestoneProject)
+    end
+
+    # GET /milestone_projects/1/edit
+    def edit; end
+
+    # POST /milestone_projects
+    def create
+      @project = current_user.projects.build(project_params.merge(type: MilestoneProject))
+
+      if @project.save
+        redirect_to [:milestones, current_namespace, @project], notice: 'Project was successfully created.'
+      else
+        render :new
+      end
+    end
+
     # GET /milestone_projects/1/milestones
     def milestones; end
 
@@ -19,6 +41,17 @@ module Freelancer
       else
         render params[:button].to_sym
       end
+    end
+
+  private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_project
+      @project = current_user.projects.find(params[:id])
+    end
+
+    def project_params
+      params.require(:milestone_project).permit(:name, :org_id)
     end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def user_clients_select_options(user)
+    user.clients.uniq.map { |client| [client.display_name, client.id] }
+  end
 end
