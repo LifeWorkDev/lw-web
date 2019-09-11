c7307b7d96649057c99b6db1f78e491ac8c3739d
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-09-10 18:32:44 -0700

    First end-to-end system test for Freelancer client/project creation

--- spec/fabricators/milestone_project_fabricator.rb
+++ spec/fabricators/milestone_project_fabricator.rb
@@ -2,6 +2,9 @@ Fabricator(:milestone_project) do
   name { Faker::Commerce.product_name }
   client(fabricator: :named_org)
   freelancer(fabricator: :user)
+end
+
+Fabricator(:milestone_project_with_milestones, from: :milestone_project) do
   milestones(fabricator: :milestone_with_comments, rand: 3..7)
 
   after_build do |project|
--- spec/system/freelancer_spec.rb
+++ spec/system/freelancer_spec.rb
@@ -11,13 +11,33 @@ RSpec.describe 'Freelancer views', type: :system do
   context "when auth'd" do
     let(:user) { Fabricate(:freelancer) }
 
-    before do
-      sign_in(user)
+    before { sign_in user }
+
+    it 'redirects from / to the freelancer project dashboard' do
       visit '/'
+      expect(page).to have_current_path('/f/projects')
     end
 
-    it 'redirects to the freelancer project dashboard' do
-      expect(page).to have_current_path('/f/projects')
+    it 'completes an entire client/project creation' do
+      visit '/f/clients/new'
+      fill_in 'org[users_attributes][0][name]', with: Faker::Name.name
+      fill_in 'org[users_attributes][0][email]', with: Faker::Internet.email
+      name = Faker::Commerce.product_name
+      fill_in 'org[projects_attributes][0][name]', with: name
+      click_on 'Continue >'
+      expect(page).to have_content('Client was successfully created.')
+      find('.flatpickr-day.today').click
+      click_on 'Continue >'
+      expect(page).to have_content('Milestones were updated.')
+      expect(page).to have_content(Milestone.last.formatted_date)
+      amount = Money.new(rand(10_000..1_000_00))
+      fill_in 'milestone_project[amount]', with: amount
+      fill_in 'milestone_project[milestones_attributes][0][amount]', with: amount
+      click_on 'Continue >'
+      expect(page).to have_content('Payments were updated.')
+      visit '/f/projects'
+      expect(page).to have_content(name)
+      expect(page).to have_content(amount.format)
     end
   end
 end
