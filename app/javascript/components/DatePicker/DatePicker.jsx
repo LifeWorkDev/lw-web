9016a19f4ec9125a8da53109deb28eabc4edd854
Gean <geankozen@gmail.com>
2019-09-11 23:20:21 -0300

    Replace stimulus-flatpickr with react-day-picker

---
+++ app/javascript/components/DatePicker/DatePicker.jsx
@@ -0,0 +1,40 @@
+import 'react-day-picker/lib/style.css'
+import DayPicker, { DateUtils } from 'react-day-picker'
+import React from 'react'
+
+import './Datepicker.scss'
+
+export default class DatePicker extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleDayClick = this.handleDayClick.bind(this)
+    this.state = {
+      selectedDays: [],
+    }
+  }
+
+  handleDayClick(day, { selected }) {
+    const { selectedDays } = this.state
+    if (selected) {
+      const selectedIndex = selectedDays.findIndex(selectedDay =>
+        DateUtils.isSameDay(selectedDay, day),
+      )
+      selectedDays.splice(selectedIndex, 1)
+    } else {
+      selectedDays.push(day)
+    }
+    this.setState({ selectedDays })
+  }
+
+  render() {
+    return (
+      <div>
+        <DayPicker
+          selectedDays={this.state.selectedDays}
+          onDayClick={this.handleDayClick}
+          numberOfMonths={2}
+        />
+      </div>
+    )
+  }
+}
