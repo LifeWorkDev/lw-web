014b349bb0d04594b5e2025eeb666790bb4b6849
Josip <polarisdev87@outlook.com>
2019-09-15 13:50:23 -0300

    Update to functional component

--- app/javascript/components/DatePicker/DatePicker.jsx
+++ app/javascript/components/DatePicker/DatePicker.jsx
@@ -1,43 +1,37 @@
 import 'react-day-picker/lib/style.css'
 import DayPicker, { DateUtils } from 'react-day-picker'
-import React from 'react'
+import PropTypes from 'prop-types'
+import React, { useState } from 'react'
 
 import './Datepicker.scss'
 
-export default class DatePicker extends React.Component {
-  constructor(props) {
-    super(props)
-    this.handleDayClick = this.handleDayClick.bind(this)
+const DatePicker = props => {
+  const [selectedDays, setSelectedDays] = useState(
+    stringsToDates(props.defaultDays),
+  )
 
-    let defaultDate = props.defaultDate
-    let selectedDays = this.stringsToDates(defaultDate)
-
-    this.state = {
-      selectedDays,
-    }
-  }
-
-  isMobile = () => {
+  const isMobile = () => {
     const result = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
       navigator.userAgent,
     )
     return result
   }
 
-  stringsToDates = dayStrings => {
-    let selectedDays = []
+  function stringsToDates(dayStrings) {
+    // const stringsToDates = dayStrings => {
+    let days = []
     let timezoneOffset = new Date().getTimezoneOffset()
     if (dayStrings != null) {
       dayStrings.map(day => {
         let date = new Date(day)
         date.setHours(date.getHours() + timezoneOffset / 60)
-        selectedDays.push(date)
+        days.push(date)
       })
     }
-    return selectedDays
+    return days
   }
 
-  displayFormatDate = date => {
+  const displayFormatDate = date => {
     let d = new Date(date),
       month = '' + (d.getMonth() + 1),
       day = '' + d.getDate(),
@@ -49,7 +43,7 @@ export default class DatePicker extends React.Component {
     return [month, day, year].join('/')
   }
 
-  shortFormatDate = date => {
+  const shortFormatDate = date => {
     let d = new Date(date),
       month = '' + (d.getMonth() + 1),
       day = '' + d.getDate(),
@@ -61,68 +55,68 @@ export default class DatePicker extends React.Component {
     return [year, month, day].join('-')
   }
 
-  removeData = index => {
-    let { selectedDays } = this.state
-    delete selectedDays[index]
-
-    this.setState({ selectedDays })
+  const removeData = index => {
+    const newSelectedDays = selectedDays.slice()
+    delete newSelectedDays[index]
+    setSelectedDays(newSelectedDays)
   }
 
-  handleDayClick(day, { selected }) {
-    const { selectedDays } = this.state
+  // const handleDayClick = (day, { selected }) => {
+  const handleDayClick = (day, { selected }) => {
+    const newSelectedDays = selectedDays.slice()
     if (selected) {
-      const selectedIndex = selectedDays.findIndex(selectedDay =>
+      const selectedIndex = newSelectedDays.findIndex(selectedDay =>
         DateUtils.isSameDay(selectedDay, day),
       )
-      selectedDays.splice(selectedIndex, 1)
+      newSelectedDays.splice(selectedIndex, 1)
     } else {
       let minDate = new Date()
       minDate.setDate(minDate.getDate() - 1)
       if (day > minDate) {
-        selectedDays.push(day)
+        newSelectedDays.push(day)
       }
     }
-    this.setState({ selectedDays })
+    setSelectedDays(newSelectedDays)
   }
 
-  render() {
-    const { selectedDays } = this.state
-    let orderedDays = selectedDays.sort(function(date1, date2) {
-      if (date1 > date2) return 1
-      if (date1 < date2) return -1
-      return 0
-    })
-    return (
-      <div className='text-align-center'>
-        <div>
-          {orderedDays.map((day, key) => (
-            <span
-              className='badge badge-primary badge-pill date-pill mr-2 mb-2'
-              key={key}
+  let orderedDays = selectedDays.sort(function(date1, date2) {
+    if (date1 > date2) return 1
+    if (date1 < date2) return -1
+    return 0
+  })
+
+  return (
+    <div className='text-align-center'>
+      <div>
+        {orderedDays.map((day, key) => (
+          <span
+            className='badge badge-primary badge-pill date-pill mr-2 mb-2'
+            key={key}
+          >
+            <span>{displayFormatDate(day)}</span>
+            <input
+              type='hidden'
+              name={'milestone_project[milestones_attributes][][date]'}
+              value={shortFormatDate(day)}
+            />
+            <button
+              type='button'
+              className='close'
+              onClick={() => removeData(key)}
             >
-              <span>{this.displayFormatDate(day)}</span>
-              <input
-                type='hidden'
-                name={'milestone_project[milestones_attributes][][date]'}
-                value={this.shortFormatDate(day)}
-              />
-              <button
-                type='button'
-                className='close'
-                onClick={() => this.removeData(key)}
-              >
-                <span>×</span>
-              </button>
-            </span>
-          ))}
-        </div>
-        <DayPicker
-          selectedDays={this.state.selectedDays}
-          onDayClick={this.handleDayClick}
-          numberOfMonths={this.isMobile() ? 1 : 2}
-          disabledDays={{ before: new Date() }}
-        />
+              <span>×</span>
+            </button>
+          </span>
+        ))}
       </div>
-    )
-  }
+      <DayPicker
+        selectedDays={selectedDays}
+        onDayClick={handleDayClick}
+        numberOfMonths={isMobile() ? 1 : 2}
+        disabledDays={{ before: new Date() }}
+      />
+    </div>
+  )
 }
+
+export default DatePicker
