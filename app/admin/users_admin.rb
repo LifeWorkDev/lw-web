94728b5526848ce9cfcc8c0a4154f45003dad76c
Muhammad Soban Akram <soban.se@gmail.com>
2019-09-20 18:53:43 +0500

    User added in admin

---
+++ app/admin/users_admin.rb
@@ -0,0 +1,45 @@
+Trestle.resource(:users) do
+  menu do
+    item :users, icon: 'fa fa-star'
+  end
+
+  # Customize the table columns shown on the index view.
+  #
+  table do
+    column :id
+    column :name
+    column :email
+    column :status
+    column :roles
+    column :org
+    column :created_at, align: :center
+    actions do |toolbar, instance, _admin|
+      toolbar.link 'Sign in as', main_app.users_impersonate_path(id: instance.id)
+      toolbar.delete
+    end
+  end
+
+  # Customize the form fields shown on the new/edit views.
+  #
+  form do |_user|
+    text_field :name
+    email_field :email
+    select :status, User.aasm.states.map(&:name)
+    # select :roles, [[]] + User::ROLES
+    phone_field :phone
+    text_field :address
+    text_field :time_zone
+    select :org_id, [[]] + Org.all.map { |org| [org.name, org.id] }
+  end
+
+  # By default, all parameters passed to the update and create actions will be
+  # permitted. If you do not have full trust in your users, you should explicitly
+  # define the list of permitted parameters.
+  #
+  # For further information, see the Rails documentation on Strong Parameters:
+  #   http://guides.rubyonrails.org/action_controller_overview.html#strong-parameters
+  #
+  # params do |params|
+  #   params.require(:user).permit(:name, ...)
+  # end
+end
