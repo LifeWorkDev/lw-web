29c1dfd24887881aa66278d430530c709a622553
Muhammad Soban Akram <soban.se@gmail.com>
2019-09-24 19:02:10 +0500

    Org model added in Admin

---
+++ app/admin/orgs_admin.rb
@@ -0,0 +1,37 @@
+Trestle.resource(:orgs) do
+  menu do
+    item :orgs, icon: 'fa fa-star'
+  end
+
+  # Customize the table columns shown on the index view.
+
+  table do
+    column :id
+    column :display_name
+    column :status
+    column :primary_contact
+    column :created_at
+  end
+
+  # Customize the form fields shown on the new/edit views.
+
+  form do |_org|
+    text_field :name
+    text_field :slug
+    select :status, Org.aasm.states.map(&:name)
+    select :work_category, []
+    text_area :work_frequency
+    text_area :metadata
+  end
+
+  # By default, all parameters passed to the update and create actions will be
+  # permitted. If you do not have full trust in your users, you should explicitly
+  # define the list of permitted parameters.
+  #
+  # For further information, see the Rails documentation on Strong Parameters:
+  #   http://guides.rubyonrails.org/action_controller_overview.html#strong-parameters
+  #
+  # params do |params|
+  #   params.require(:org).permit(:name, ...)
+  # end
+end
