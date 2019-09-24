e01d2fd4dc7968330961f41438a345efb051be63
Muhammad Soban Akram <soban.se@gmail.com>
2019-09-24 18:07:00 +0500

    user roles multi selection added

--- app/admin/users_admin.rb
+++ app/admin/users_admin.rb
@@ -25,11 +25,19 @@ Trestle.resource(:users) do
     text_field :name
     email_field :email
     select :status, User.aasm.states.map(&:name)
-    # select :roles, [[]] + User::ROLES
+    select :roles, User::ROLES, {}, multiple: true
     phone_field :phone
     text_field :address
     text_field :time_zone
     select :org_id, [[]] + Org.all.map { |org| [org.name, org.id] }
+  end
+
+  controller do
+    before_action :remove_blank_role, only: %i[create update]
+
+    def remove_blank_role
+      params[:user][:roles].delete('')
+    end
   end
 
   # By default, all parameters passed to the update and create actions will be
