5c6de3ab00db9a812c992b84edc52ffc32f15757
Muhammad Soban Akram <soban.se@gmail.com>
2019-09-24 19:22:32 +0500

    Project model added

---
+++ app/admin/projects_admin.rb
@@ -0,0 +1,41 @@
+Trestle.resource(:projects) do
+  menu do
+    item :projects, icon: 'fa fa-star'
+  end
+
+  # Customize the table columns shown on the index view.
+  #
+  table do
+    column :id
+    column :name
+    column :status
+    column :type
+    column :freelancer
+    column :client
+    column :created_at
+    actions
+  end
+
+  # Customize the form fields shown on the new/edit views.
+  #
+  form do |_project|
+    text_field :name
+    text_field :slug
+    select :client, Org.all.map { |org| [org.name, org.id] }
+    select :freelancer, User.all.map { |user| [user.name, user.id] }
+    select :status, Project.aasm.states.map(&:name)
+    number_field :amount_cents
+    text_field :currency
+  end
+
+  # By default, all parameters passed to the update and create actions will be
+  # permitted. If you do not have full trust in your users, you should explicitly
+  # define the list of permitted parameters.
+  #
+  # For further information, see the Rails documentation on Strong Parameters:
+  #   http://guides.rubyonrails.org/action_controller_overview.html#strong-parameters
+  #
+  # params do |params|
+  #   params.require(:project).permit(:name, ...)
+  # end
+end
