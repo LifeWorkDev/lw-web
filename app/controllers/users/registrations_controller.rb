ef80ed9291180a3c30204e073bfa8107a38e9727
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-10-01 21:33:29 -0700

    Fix redirect after signup/login

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,6 +11,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
 protected
 
+  def after_sign_up_path_for(_resource)
+    edit_freelancer_user_path
+  end
+
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name])
   end
