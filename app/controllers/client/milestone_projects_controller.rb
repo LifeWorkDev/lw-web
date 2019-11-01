f2e266574d9a9c7e7942a252e2548a153f24d473
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-11-01 15:52:22 -0700

    Add milestone id to stripe payment metadata

--- app/controllers/client/milestone_projects_controller.rb
+++ app/controllers/client/milestone_projects_controller.rb
@@ -9,6 +9,9 @@ class Client::MilestoneProjectsController < MilestoneProjectsController
       currency: amount.currency.to_s,
       customer: @project.client.stripe_id,
       source: @project.client.primary_pay_method.stripe_id,
+      metadata: {
+        'Milestone ID': milestone.id,
+      },
     )
     FreelancerMailer.deposit_received(user: @project.freelancer, project: @project, amount: milestone.amount.format).deliver_later
     redirect_to [current_namespace, Project], notice: "Your deposit was received. #{@project.freelancer.name} has been notified so they can start work on your project."
