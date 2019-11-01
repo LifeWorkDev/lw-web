f460a0b67ea2a01d7433609cc67c6361dfbb43b7
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-11-01 16:34:04 -0700

    Enable mailer previews in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,5 +1,8 @@
 require File.expand_path('production.rb', __dir__)
 
-Rails.application.configure do
-  # overrides
+Rails.application.configure do # overrides
+  # Enable mailer previews
+  config.action_mailer.show_previews = true
+  config.action_mailer.preview_path = "#{::Rails.root}/spec/mailers/previews"
+  ActiveSupport::Dependencies.autoload_paths << config.action_mailer.preview_path
 end
