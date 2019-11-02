d6cddc61919f6e17294858e2173d690ed538d399
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-11-01 17:38:47 -0700

    Rename client invite translation var from user to freelancer to avoid confusion

--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -5,7 +5,7 @@ class ClientMailer < ApplicationMailer
     Time.use_zone(user.time_zone) do
       @project = project
       @get_started_url = user.raw_invitation_token.present? ? accept_user_invitation_url(invitation_token: user.raw_invitation_token) : [:payments, :client, project]
-      make_bootstrap_mail(to: user.email, subject: t('.subject', user: project.freelancer.name))
+      make_bootstrap_mail(to: user.email, subject: t('.subject', freelancer: project.freelancer.name))
     end
   end
 end
