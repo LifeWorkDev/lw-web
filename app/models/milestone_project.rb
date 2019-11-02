7a6d3efcb89612246f4a3b46ea9a6822e8f21e33
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-11-01 18:05:07 -0700

    Make existing_milestone a ? method

--- app/models/milestone_project.rb
+++ app/models/milestone_project.rb
@@ -1,11 +1,11 @@
 class MilestoneProject < Project
   has_many :milestones, -> { order(:date) }, dependent: :destroy, foreign_key: :project_id, inverse_of: :project
   has_many :comments, through: :milestones
-  accepts_nested_attributes_for :milestones, reject_if: :existing_milestone, allow_destroy: true
+  accepts_nested_attributes_for :milestones, reject_if: :existing_milestone?, allow_destroy: true
 
 private
 
-  def existing_milestone(milestone_attrs)
+  def existing_milestone?(milestone_attrs)
     return true if milestones.find_by(date: milestone_attrs[:date])
 
     false
