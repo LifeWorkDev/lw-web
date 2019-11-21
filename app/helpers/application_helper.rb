1a89b68c7f1d303d96581da7fae91406635cd7d5
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-11-21 12:04:22 -0800

    Use material design icons from npm instead of gem [Delivers #169212464]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def mdi_svg(icon, options = {})
+    options[:width] ||= 24
+    content_tag(:svg, options.merge(viewBox: '0 0 24 24')) do
+      content_tag(:use, nil, href: "#{asset_pack_path("media/svg/#{icon}.svg")}#mdi-#{icon}")
+    end
+  end
 end
