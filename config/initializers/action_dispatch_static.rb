2580d2347339f4f79249c41023645a9e4024f016
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-11-21 17:09:20 -0800

    Serve gzipped svgs

---
+++ config/initializers/action_dispatch_static.rb
@@ -0,0 +1,16 @@
+require 'action_dispatch/middleware/static'
+
+ActionDispatch::FileHandler.class_eval do
+private
+
+  def gzip_file_path(path)
+    return false if %w[image/png image/jpeg image/gif image/x-icon image/vnd.microsoft.icon].include? content_type(path)
+
+    gzip_path = "#{path}.gz"
+    if File.exist?(File.join(@root, ::Rack::Utils.unescape_path(gzip_path)))
+      gzip_path
+    else
+      false
+    end
+  end
+end
