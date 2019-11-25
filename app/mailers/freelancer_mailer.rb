906041ce67122e36f665a1bba804aa00551b6549
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-11-25 09:35:52 -0800

    Freelancer milestone approaching email

--- app/mailers/freelancer_mailer.rb
+++ app/mailers/freelancer_mailer.rb
@@ -5,4 +5,13 @@ class FreelancerMailer < ApplicationMailer
       make_bootstrap_mail(to: user.email, subject: t('.subject', project: project.name))
     end
   end
+
+  def milestone_approaching(user:, milestone:)
+    Time.use_zone(user.time_zone) do
+      @client_name = milestone.client.display_name
+      @milestone = milestone
+      @user = user
+      make_bootstrap_mail(to: user.email, reply_to: milestone.comment_reply_address, subject: t('.subject', project: milestone.project))
+    end
+  end
 end
--- spec/mailers/previews/freelancer_mailer_preview.rb
+++ spec/mailers/previews/freelancer_mailer_preview.rb
@@ -2,4 +2,8 @@ class FreelancerMailerPreview < ActionMailer::Preview
   def deposit_received
     FreelancerMailer.deposit_received(user: User.first, project: Project.first, amount: Milestone.first.amount)
   end
+
+  def milestone_approaching
+    FreelancerMailer.milestone_approaching(user: User.first, milestone: Milestone.first)
+  end
 end
