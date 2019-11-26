46cb418d7706d1ea86f6a0de711f49ece2621cc6
Stefan Wrobel <swrobel@users.noreply.github.com>
2019-11-26 14:23:05 -0800

    Serve SVGs off CDN instead of our app. Updage gems & packages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 module ApplicationHelper
   def mdi_svg(icon, options = {})
-    options[:width] ||= 24
-    content_tag(:svg, options.merge(viewBox: '0 0 24 24')) do
-      content_tag(:use, nil, href: "#{asset_pack_path("media/svg/#{icon}.svg")}#mdi-#{icon}")
-    end
+    options[:width] ||= options[:height] ||= options.delete(:size) || 24
+    image_tag "https://cdn.jsdelivr.net/npm/@mdi/svg@4.6.95/svg/#{icon}.svg#mdi-#{icon}", options.merge(onload: 'SVGInject(this)')
   end
 end
