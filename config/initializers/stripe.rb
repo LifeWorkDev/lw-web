82730b221b39ab4ce86dce719cd7bf5a5ec46324
Stefan Wrobel <swrobel@users.noreply.github.com>
2020-01-10 15:38:29 -0800

    Raise Stripe ACH limit to $10k

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -2,6 +2,6 @@ Stripe.api_key = Rails.application.credentials.stripe[:secret_key]
 Stripe.client_id = Rails.application.credentials.stripe[:client_id]
 Stripe.max_network_retries = 2
 Stripe.log_level = Stripe::LEVEL_INFO if Rails.env.development?
-ACH_MAX ||= Money.new(5_000_00)
+ACH_MAX ||= Money.new(10_000_00)
 LIFEWORK_FEE ||= 0.00
 ACH_MAX_BEFORE_FEE ||= ACH_MAX * (1 - LIFEWORK_FEE)
