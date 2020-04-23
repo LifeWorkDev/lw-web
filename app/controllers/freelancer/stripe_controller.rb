2fd35ad314a938e0c3b187723fb5be90ae6b51f8
Stefan Wrobel <swrobel@users.noreply.github.com>
2020-04-23 11:27:20 -0700

    Allow Freelancer Stripe connect without a project

--- app/controllers/freelancer/stripe_controller.rb
+++ app/controllers/freelancer/stripe_controller.rb
@@ -8,7 +8,12 @@ class Freelancer::StripeController < AuthenticatedController
     end
     if stripe_resp
       current_user.update!(stripe_id: stripe_resp.stripe_user_id, stripe_access_token: stripe_resp.access_token, stripe_key: stripe_resp.stripe_publishable_key, stripe_refresh_token: stripe_resp.refresh_token)
-      redirect_to next_step(current_user.projects.first), notice: 'Successfully linked to Stripe.'
+      redirect = if (project = current_user.projects.first)
+                   next_step(project)
+                 else
+                   user_default_path
+                 end
+      redirect_to redirect, notice: 'Successfully linked to Stripe.'
     else
       redirect_to freelancer_stripe_connect_path, alert: 'Could not link to Stripe. Please try again.'
     end
