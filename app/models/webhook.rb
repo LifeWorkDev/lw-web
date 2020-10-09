b11cf8c417b5ad8aa91dc9c1f015eb0117a5dc90
Stefan Wrobel <swrobel@users.noreply.github.com>
2020-10-09 15:24:47 -0700

    Don't attempt to mark disbursed payments as succeeded

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -15,9 +15,11 @@ class Webhook < ApplicationRecord
             case event
             when "charge.succeeded"
               payment_id = data.dig("data", "object", "id")
+              payment = Payment.find_by(stripe_id: payment_id)
+              raise "No payment found with stripe id #{payment_id}" if !payment && Rails.env.production?
 
               if payment_id.start_with? "py_" # ACH payments only
-                Payment.find_by(stripe_id: payment_id)&.succeed!
+                payment&.succeed! unless payment&.disbursed?
               else true; end
             else
               raise "No handler for Stripe event #{event}"
