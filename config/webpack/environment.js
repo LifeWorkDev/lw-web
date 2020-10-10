466d459800fe80db3bdc09d180f7bc57d9c4fbdf
Stefan Wrobel <swrobel@users.noreply.github.com>
2020-10-09 17:47:51 -0700

    Bootstrap Native 3.x, fix right border on nav items on mobile

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,13 +2,4 @@ const { environment } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
 
 environment.loaders.prepend('erb', erb)
-environment.loaders.append('bootstrap-native', {
-  test: /bootstrap\.native/,
-  use: {
-    loader: 'bootstrap.native-loader',
-    options: {
-      only: ['collapse'],
-    },
-  },
-})
 module.exports = environment
