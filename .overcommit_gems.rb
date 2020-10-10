465df203c9891c3e9dfde015bd1c5618c99d45cd
Stefan Wrobel <swrobel@users.noreply.github.com>
2020-10-09 17:33:46 -0700

    Bundler 2.1.4 (new Heroku default)

--- .overcommit_gems.rb
+++ .overcommit_gems.rb
@@ -1,15 +1,15 @@
-source "https://rubygems.org"
+source "https://rubygems.org" do
+  group :development do
+    gem "overcommit" # Git commit hooks
+    gem "rubocop-daemon", require: false # Run rubocop as a daemon so it starts up faster
+  end
 
-group :development do
-  gem "overcommit" # Git commit hooks
-  gem "rubocop-daemon", require: false # Run rubocop as a daemon so it starts up faster
-end
-
-group :development, :test do
-  gem "rubocop", require: false
-  gem "rubocop-performance", require: false
-  gem "rubocop-rails", require: false
-  gem "rubocop-rspec", require: false
-  gem "slim_lint", require: false
-  gem "standard", ">= 0.3", require: false
+  group :development, :test do
+    gem "rubocop", require: false
+    gem "rubocop-performance", require: false
+    gem "rubocop-rails", require: false
+    gem "rubocop-rspec", require: false
+    gem "slim_lint", require: false
+    gem "standard", ">= 0.3", require: false
+  end
 end
