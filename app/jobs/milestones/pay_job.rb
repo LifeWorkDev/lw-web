e93132aaa45600c0cf2b1f2626276a79538282b1
Stefan Wrobel <swrobel@users.noreply.github.com>
2020-10-25 11:18:22 -0700

    AASM cares about grammar!

--- app/jobs/milestones/pay_job.rb
+++ app/jobs/milestones/pay_job.rb
@@ -2,7 +2,7 @@ module Milestones
   class PayJob < Job
     def perform(milestone)
       super
-      raise "Can't pay Milestone #{milestone.id} because it's in state #{milestone.status}" unless milestone.can_pay?
+      raise "Can't pay Milestone #{milestone.id} because it's in state #{milestone.status}" unless milestone.may_pay?
       raise "Can't pay Milestone #{milestone.id} before its payment date #{milestone.formatted_date}" unless milestone.payable?
 
       milestone.pay!
