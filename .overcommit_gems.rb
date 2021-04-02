d6c6dfe76a5cd76c28f045a97af0274e2a67560c
Stefan Wrobel <swrobel@users.noreply.github.com>
2021-04-02 16:07:53 -0700

    Update gems & packages

--- .overcommit_gems.rb
+++ .overcommit_gems.rb
@@ -10,6 +10,6 @@ source "https://rubygems.org" do
     gem "rubocop-rails", require: false
     gem "rubocop-rspec", "~> 2.2.0", require: false
     gem "slim_lint", require: false
-    gem "standard", ">= 0.3", require: false
+    gem "standard", "> 1.0", require: false
   end
 end
