ae1efd4f5b9e00401bdd4be6ad90d36602f4f862
Stefan Wrobel <swrobel@users.noreply.github.com>
2021-11-06 15:59:02 -0700

    Reconciliation report should show freelancer amount

--- app/controllers/freelancer/reports_controller.rb
+++ app/controllers/freelancer/reports_controller.rb
@@ -6,12 +6,13 @@ class Freelancer::ReportsController < AuthenticatedController
 
   def payments
     csv_data = CSV.generate(headers: true) do |csv|
-      csv << ["Deposited", "Amount", "Status", "Project", "Client", "Disbursed", "Received", "Total payout amount"]
+      csv << ["Deposited", "Amount", "Fee", "Status", "Project", "Client", "Disbursed", "Received", "Total payout amount"]
       current_user.payments_received.includes(:disbursement_line, :payout_line).successful.find_each do |p|
         payout_metadata = p.payout_line&.metadata
         csv << [
           p.paid_at,
-          p.amount,
+          p.freelancer_amount,
+          p.freelancer_fee,
           p.status,
           p.project,
           p.client,
