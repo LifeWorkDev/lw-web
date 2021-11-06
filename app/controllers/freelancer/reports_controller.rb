6f2e4fe2b56af681704def5d91f9d8f193372883
Stefan Wrobel <swrobel@users.noreply.github.com>
2021-11-06 15:47:40 -0700

    Fix reconciliation report with non-disbursed payments

--- app/controllers/freelancer/reports_controller.rb
+++ app/controllers/freelancer/reports_controller.rb
@@ -8,15 +8,16 @@ class Freelancer::ReportsController < AuthenticatedController
     csv_data = CSV.generate(headers: true) do |csv|
       csv << ["Deposited", "Amount", "Status", "Project", "Client", "Disbursed", "Received", "Total payout amount"]
       current_user.payments_received.includes(:disbursement_line, :payout_line).successful.find_each do |p|
+        payout_metadata = p.payout_line&.metadata
         csv << [
           p.paid_at,
           p.amount,
           p.status,
           p.project,
           p.client,
-          p.disbursement_line.created_at,
-          p.payout_line.metadata["arrival_date"],
-          Money.new(p.payout_line.metadata["amount"]),
+          p.disbursement_line&.created_at,
+          payout_metadata&.dig("arrival_date"),
+          Money.new(payout_metadata&.dig("amount")),
         ]
       end
     end
