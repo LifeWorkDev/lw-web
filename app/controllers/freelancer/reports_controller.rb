e037b3492057de90a7e16769cb37bf5868c8fb21
Stefan Wrobel <swrobel@users.noreply.github.com>
2021-11-07 08:34:33 -0800

    Reconciliation report enhancements

--- app/controllers/freelancer/reports_controller.rb
+++ app/controllers/freelancer/reports_controller.rb
@@ -7,8 +7,9 @@ class Freelancer::ReportsController < AuthenticatedController
   def payments
     csv_data = CSV.generate(headers: true) do |csv|
       csv << ["Deposited", "Amount", "Fee", "Status", "Project", "Client", "Disbursed", "Received", "Total payout amount"]
-      current_user.payments_received.includes(:disbursement_line, :payout_line).successful.find_each do |p|
+      current_user.payments_received.includes(:disbursement_line, :payout_line).successful.find_each(order: :desc) do |p|
         payout_metadata = p.payout_line&.metadata
+        payout_amount = payout_metadata&.dig("amount")
         csv << [
           p.paid_at,
           p.freelancer_amount,
@@ -18,7 +19,7 @@ class Freelancer::ReportsController < AuthenticatedController
           p.client,
           p.disbursement_line&.created_at,
           ActiveSupport::JSON.send(:convert_dates_from, payout_metadata&.dig("arrival_date")),
-          Money.new(payout_metadata&.dig("amount")),
+          payout_amount ? Money.new(payout_amount) : nil,
         ]
       end
     end
